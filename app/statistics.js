68d52a2e4f3b7285899f34eb643756ef9cec3693
zak <31302217+kmzakiya@users.noreply.github.com>
2022-05-09 16:41:33 +0530

    Update statistics.js
    Implemented min, math, average

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,15 @@
 module.exports=function getStatistics(numbers){
-    
+  if (!numbers || !numbers.length) {
+    return {
+      min: NaN,
+      max: NaN,
+      average: NaN,
+    };
+  }
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+  const average = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  return { min, max, average };
     //implement the computation of statistics here
 }
 
